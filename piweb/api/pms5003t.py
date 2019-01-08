31508083c43eb502131c81dac5b53ed0a05a1a3b
chengyimbp <chengyi.wu@outlook.com>
2019-01-08 21:22:04 +0800

    Update pms5003t.py
    - Make it work with Py3

--- piweb/api/pms5003t.py
+++ piweb/api/pms5003t.py
@@ -3,60 +3,42 @@ https://github.com/Thomas-Tsai/pms3003-g3
 '''
 
 import serial
-import time
-import sys
-from struct import *
+from collections import OrderedDict
 
 class Sensor():
-    def __init__(self, tty = '/dev/ttyS0', debug = True):
-        self.debug = debug
-        self.edian = sys.byteorder
+    def __init__(self, tty = '/dev/ttyS0'):
         self.tty = tty
     
     def conn(self, device):
         self.serial = serial.Serial(device, baudrate = 9600)
-    
-    def check_keyword(self):
-        while True:
-            token = self.serial.read()
-            token_hex = token.encode('hex')
-            if token_hex == '42':
-                token = self.serial.read()
-                token_hex = token.encode('hex')
-                return token_hex == '4d'
-            else:
-                return False
 
     def read_word(self, data, idx):
-        idx *= 4
-        return int(data[idx : idx + 4], 16)
+        return int(data[idx] + data[idx + 1], 16)
 
     def read_data(self):
         data = self.serial.read(32)
-        data = data.encode('hex')
-        if self.debug:
-            print(data)
+        data = ["{:02X}".format(d) for d in data]
         
-        res = {}
-        res['pm1_cf'] = self.read_word(data, 1)
-        res['pm25_cf'] = self.read_word(data, 2)
-        res['pm10_cf'] = self.read_word(data, 3)
-        res['pm1'] = self.read_word(data, 4)
-        res['pm25'] = self.read_word(data, 5)
-        res['pm10'] = self.read_word(data, 6)
+        if data[0] != '42' or data[1] != '4D':
+            return None
+        
+        res = OrderedDict()
+        res['pm1_cf'] = self.read_word(data, 4)
+        res['pm25_cf'] = self.read_word(data, 6)
+        res['pm10_cf'] = self.read_word(data, 8)
+        res['pm1'] = self.read_word(data, 10)
+        res['pm25'] = self.read_word(data, 12)
+        res['pm10'] = self.read_word(data, 14)
 
-        res['temperature'] = self.read_word(data, 11) / 10
-        res['humidity'] = self.read_word(data, 12) / 10
+        res['temperature'] = self.read_word(data, 24) / 10
+        res['humidity'] = self.read_word(data, 26) / 10
         return res
 
     def read(self):
         self.conn(self.tty)
-        if self.check_keyword():
-            return self.read_data()
-        return None
-
-    def close(self):
+        data = self.read_data()
         self.serial.close()
+        return data
 
 if __name__ == '__main__':
     '''
@@ -65,4 +47,3 @@ if __name__ == '__main__':
     sensor = Sensor()
     data = sensor.read()
     print(data)
-    sensor.close()
