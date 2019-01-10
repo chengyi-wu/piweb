17cd14c817b8511d0b2ea33e537b8f0baa9a91e7
chenwu <chengyi.wu@outlook.com>
2019-01-10 11:32:20 +0800

    + fix specs
    + fix specs

--- piweb/api/pms5003t.py
+++ piweb/api/pms5003t.py
@@ -3,22 +3,22 @@
 | ------ |:------------------------:| --------------------------------------------------------------------:|
 | 0      | Start byte 1             | Fixed 0x42 (char "B")                                                |
 | 1      | Start byte 2             | Fixed 0x4D (char "M")                                                |
-| 2      | Frame length (16 bits)   | Length = 13 * 2 + 2 (data + checksum)                                |
+| 2      | Frame length (16 bits)   | Length = 14 * 2                                                      |
 | 4      | Data 1 (16 bits)         | The PM1.0 concentration (CF = 1, standard particle), unit μg/m³      |
 | 6      | Data 2 (16 bits)         | The PM2.5 concentration (CF = 1, standard particle), unit μg/m³      |
 | 8      | Data 3 (16 bits)         | The PM10 concentration (CF = 1, standard particle), unit μg/m³       |
 | 10     | Data 4 (16 bits)         | The PM1.0 concentration (generic atmospheric conditions), unit μg/m³ |
 | 12     | Data 5 (16 bits)         | The PM2.5 concentration (generic atmospheric conditions), unit μg/m³ |
 | 14     | Data 6 (16 bits)         | The PM10 concentration (generic atmospheric conditions), unit μg/m³  |
-| 16     | Data 7 (16 bits)         | The number of particles with diameter => 0.3 μm in 0.1 liter of air  |
-| 18     | Data 8 (16 bits)         | The number of particles with diameter => 0.5 μm in 0.1 liter of air  |
-| 20     | Data 9 (16 bits)         | The number of particles with diameter => 1.0 μm in 0.1 liter of air  |
-| 22     | Data 10 (16 bits)        | The number of particles with diameter => 2.5 μm in 0.1 liter of air  |
+| 16     | Data 7 (16 bits)         | The number of particles with diameter >= 0.3 μm in 0.1 liter of air  |
+| 18     | Data 8 (16 bits)         | The number of particles with diameter >= 0.5 μm in 0.1 liter of air  |
+| 20     | Data 9 (16 bits)         | The number of particles with diameter >= 1.0 μm in 0.1 liter of air  |
+| 22     | Data 10 (16 bits)        | The number of particles with diameter >= 2.5 μm in 0.1 liter of air  |
 | 24     | Data 11 (16 bits)        | Temperature. Note: Real temperature = value / 10                     |
 | 26     | Data 12 (16 bits)        | Humidity. Note: Real humidity = value / 10                           |
-| 28     | Data 13 (8 bits)         | Version No.                                                          |
-| 29     | Data 13 (8 bits)         | Error No.                                                            |
-| 30     | Data 14 (16 bits)        | Checksum = sum(data[0:30])                                           |
+| 28     | Data 13 (high 8 bits)    | Version No.                                                          |
+| 29     | Data 13 (low 8 bits)     | Error No.                                                            |
+| 30     | Data checksum (16 bits)  | Checksum = byte 0 + byte 1 + ... + byte 29                           |
 '''
 
 import serial
